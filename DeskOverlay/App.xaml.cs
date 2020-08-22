db558b53a57535387c8d428a816b9fdd3935cfec
Sreejith <SreejithSm@gmail.com>
2020-08-22 22:05:14 +0200

    making singleInstance with mutex

--- DeskOverlay/App.xaml.cs
+++ DeskOverlay/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.ComponentModel;
 using Newtonsoft.Json.Linq;
+using System.Threading;
 
 namespace DeskOverlay
 {
@@ -16,11 +17,32 @@ namespace DeskOverlay
 
     public partial class App : Application
     {
+
+        private Mutex _instanceMutex = null;
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_instanceMutex != null)
+                _instanceMutex.ReleaseMutex();
+            base.OnExit(e);
+        }
+
+
         private System.Windows.Forms.NotifyIcon _notifyIcon;
         private bool _isExit;
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            // check that there is only one instance of the control panel running...
+            bool createdNew;
+            _instanceMutex = new Mutex(true, @"DeskOverlay.SMFrameworks.linwinsoft.com", out createdNew);
+            if (!createdNew)
+            {
+                _instanceMutex = null;
+                Application.Current.Shutdown();
+                return;
+            }
+
             base.OnStartup(e);
             getMainWindowToShow();
             MainWindow.Closing += MainWindow_Closing;
